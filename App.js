34c7595210428e3747a08bfc6bcca74b12d4c4e6
Djafer Abdelhadi <41378212+asotjrs@users.noreply.github.com>
2021-09-08 04:09:59 +0200

    final touches

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import CarItem from "./component/CarItem";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem />
+      <Header />
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
